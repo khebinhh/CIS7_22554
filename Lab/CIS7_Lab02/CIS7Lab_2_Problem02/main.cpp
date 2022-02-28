101cdc6c761d501ae23626fb12c84fa33ef7739f
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-02-27 20:53:56 -0800

    problem 2

--- Lab/CIS7_Lab02/CIS7Lab_2_Problem02/main.cpp
+++ Lab/CIS7_Lab02/CIS7Lab_2_Problem02/main.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
+    std::cout << false << std::endl;    //true evaluates to 1
+    std::cout << !false << std::endl;   //!true evaluates to 0
+    
+    bool b(true);
+    std::cout << b << std::endl;        // b is false, which evaluates to 0
+    std::cout << !b << std::endl;       // !b is true, which evaluates to 1
     return 0;
 }
