04b0ca2f775bf9772c2e8936e581e870a05ed9f7
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-02-27 17:02:03 -0800

    format changes

--- Lab/Cis7Lab_2_Problem01/main.cpp
+++ Lab/Cis7Lab_2_Problem01/main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-    unsigned int p = 40;        //binary 40 = 101000
-    unsigned int q = 17;        //binary 40 =  10001
+    unsigned int p = 40;                                //binary 40 = 101000
+    unsigned int q = 17;                                //binary 40 =  10001
     int result = 0;
 
     result = p & q;                                     //binary 0 = 000000
@@ -15,7 +15,7 @@ int main(){
     result = p ^ q;                                     //binary 57 = 111001
     cout << "Result of P^Q is : " << result << endl;    //note that binary 1 did not overlap
 
-result = ~p;                                            //binary 23 = 10111
+    result = ~p;                                        //binary 23 = 10111
     cout << "Result of ~P is : " << result << endl;
 
     result = ~(p & q);
