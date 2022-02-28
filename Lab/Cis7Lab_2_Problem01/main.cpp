0ff28a3aacd304b722a07ca66d79a169c321dfa1
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-02-27 17:00:06 -0800

    Sun Feb 27

--- Lab/Cis7Lab_2_Problem01/main.cpp
+++ Lab/Cis7Lab_2_Problem01/main.cpp
@@ -6,16 +6,16 @@ int main(){
     unsigned int q = 17;        //binary 40 =  10001
     int result = 0;
 
-    result = p & q;
+    result = p & q;                                     //binary 0 = 000000
     cout << "Result of P&Q is : " << result << endl;
     
-    result = p | q;
+    result = p | q;                                     //binary 57 = 111001
     cout << "Result of P|Q is : " << result << endl;
 
-    result = p ^ q;
-    cout << "Result of P^Q is : " << result << endl;
+    result = p ^ q;                                     //binary 57 = 111001
+    cout << "Result of P^Q is : " << result << endl;    //note that binary 1 did not overlap
 
-    result = ~p;
+result = ~p;                                            //binary 23 = 10111
     cout << "Result of ~P is : " << result << endl;
 
     result = ~(p & q);
