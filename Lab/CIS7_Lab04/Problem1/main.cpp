83a9cdf907ce1f097f3cd0b085516b0e503fa0de
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-03-16 15:33:43 -0700

    yeet

--- Lab/CIS7_Lab04/Problem1/main.cpp
+++ Lab/CIS7_Lab04/Problem1/main.cpp
@@ -11,13 +11,16 @@ int main(){
     int i=1;
     do{
         //print the elements in the arrays
-        std::cout << i << ": " << myints[0] << ' ' << myints[1] << " " << myints[2] << " " << myints[3] << '\n';
+        std::cout << i << ": " << myints[0] << ' ' 
+            << myints[1] << " " << myints[2] << " " << myints[3] << '\n';
         i++;
         //permutation stops when array is in descending order
-        //if the while condition is flase at the first time, the array will be sorted ascending regardless.
+        //if the while condition is flase at the first time, 
+        //the array will be sorted ascending regardless.
     }
     while(std::next_permutation(myints, myints+4));
 
-    std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << ' ' << myints[3] << ' ' << '\n';
+    std::cout << "After loop: " << myints[0] << ' ' 
+        << myints[1] << ' ' << myints[2] << ' ' << myints[3] << ' ' << '\n';
     return 0;
 }
