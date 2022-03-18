ba600a40dce6a76645adefb5b870849bd20444cb
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-03-18 16:05:46 -0700

    update

--- Lab/CIS7_Lab04/Problem2/main.cpp
+++ Lab/CIS7_Lab04/Problem2/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <algorithm>    //permutation
+#include <algorithm>
 
 int main(){
-    int myints[] = {21,32,3,4};
-    //sort the array in ascending order up to 4 elements
+    int myints[] = {1,2,3,4};
+
     std::sort (myints, myints+4);
+    std::reverse (myints, myints+4);
 
     std::cout << "The 4! possible permutations with 4 elements:\n";
 
     int i=1;
     do{
-        //print the elements in the arrays
         std::cout << i << ": " << myints[0] << ' ' 
             << myints[1] << " " << myints[2] << " " << myints[3] << '\n';
         i++;
-        //permutation stops when array is in descending order
-        //if the while condition is flase at the first time, 
-        //the array will be sorted ascending regardless.
     }
-    while(std::next_permutation(myints, myints+4));
+    while(std::prev_permutation(myints, myints+4));
 
     std::cout << "After loop: " << myints[0] << ' ' 
         << myints[1] << ' ' << myints[2] << ' ' << myints[3] << ' ' << '\n';
