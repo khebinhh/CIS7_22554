e34f468ac636d20e6fc25da6cb9a7280d1dd1caa
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-03-19 17:24:55 -0700

    update

---
+++ Lab/CIS7_Lab04/Problem4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void permute(string str, string out){
+    // When size of str becomes 0, out has a
+    // permutation ( length of out is n )
+
+    if (str.size() == 0){
+        cout << out << endl;
+        return;
+    }
+    // One be one move all characters at
+    // the beginning of out (or result)
+    for (int i = 0; i < str.size(); i++){
+        // Remove first character from str and
+        // add it in out
+        permute (str.substr(1), out + str[0]);
+
+        // Rotate string in a way second character
+        // moves to the beginning
+        rotate(str.begin(), str.begin() + 1, str.end());
+    }
+}
+
+// Driver code
+int main(){
+    string str = "ABCDEF";
+    permute(str, "");
+    return 0;
+}
