38db5435d4d2bd4f3d5bb97c66590d32bf3e5092
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-04-02 21:36:44 -0700

    update

--- Lab/CIS7_Lab06/Problem1/main.cpp
+++ Lab/CIS7_Lab06/Problem1/main.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
+/* function main begins program execution */
+int main(){
+    int i;  //counter
+            //loop 20 times
+    for (i =1; i <= 20; i++){
+        //pick random number from 1 to 6 and output it
+        cout << (1 + (rand() % 6));
+        //if counter is divisible by 5, begin new line of output
+        if (i % 5 == 0){
+            cout << ("\n");
+        }
+    }
+    return 0;
+}
