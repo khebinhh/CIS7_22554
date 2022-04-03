7cab919ba941a40a31c85c8e2f51993c8b33d9d6
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-04-02 22:32:22 -0700

    bruh

--- Lab/CIS7_Lab06/Problem1/main.cpp
+++ Lab/CIS7_Lab06/Problem1/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 /* function main begins program execution */
 int main(){
     int i;  //counter
-            //loop 20 times
-    for (i =1; i <= 20; i++){
-        //pick random number from 1 to 6 and output it
-        cout << (1 + (rand() % 6));
+            //loop 40 times
+    for (i =1; i <= 40; i++){
+        //pick random number from 1 to 8 and output it
+        cout << (1 + (rand() % 8));
         //if counter is divisible by 5, begin new line of output
         if (i % 5 == 0){
             cout << ("\n");
--- Lab/CIS7_Lab06/Problem2/main.cpp
+++ Lab/CIS7_Lab06/Problem2/main.cpp
@@ -1,1 +1,48 @@
 #include <iostream>
+#include <stdlib.h>
+using namespace std;
+//function main begins program execution
+int main(){
+    int frequency1 = 0;
+    int frequency2 = 0;
+    int frequency3 = 0;
+    int frequency4 = 0;
+    int frequency5 = 0;
+    int frequency6 = 0;
+    int roll;
+    int face;
+    //loop 6000 times
+    for (roll = 1; roll <= 6000; roll++){
+        face = 1 + rand() % 6;
+        //determine face value and increment appropriate counter
+        switch (face){
+            case 1:
+                ++frequency1;
+                break;
+            case 2:
+                ++frequency2;
+                break;
+            case 3:
+                ++frequency3;
+                break;
+            case 4:
+                ++frequency4;
+                break;
+            case 5:
+                ++frequency5;
+                break;
+            case 6:
+                ++frequency6;
+                break;
+        }
+    }
+
+    cout << "\n" << "Face   " << "Frequency";
+    cout << "\n" << "Face 1 " << (frequency1);
+    cout << "\n" << "Face 2 " << (frequency2);
+    cout << "\n" << "Face 3 " << (frequency3);
+    cout << "\n" << "Face 4 " << (frequency4);
+    cout << "\n" << "Face 5 " << (frequency5);
+    cout << "\n" << "Face 6 " << (frequency6);
+    return 0;
+}
