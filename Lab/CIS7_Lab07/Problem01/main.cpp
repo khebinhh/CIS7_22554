d58b08fd32e171ebb6ff3a3722be8fbee524cbec
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-04-16 20:45:29 -0700

    final update

--- Lab/CIS7_Lab07/Problem01/main.cpp
+++ Lab/CIS7_Lab07/Problem01/main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 // otherwise -1
 
 int binarySearch(int arr[], int l, int r, int x){
-    while (1 <= r){
-        int m = 1 + (r - 1) / 2;
+    while (l <= r){
+        int m = l + (r - l) / 2;
 
         //check if x is present at mmid
         if (arr[m] == x)
@@ -29,7 +29,7 @@ int binarySearch(int arr[], int l, int r, int x){
 }
 
 int main(){
-    int arr[] = {2,  3, 4, 10, 40};
+    int arr[] = { 2, 3, 4, 10, 40 };
     int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
     int result = binarySearch (arr, 0, n - 1, x);
