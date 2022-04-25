c0ea991b353cee98b2162a7b9cfc63dfddb3dfcc
Kevin Ramirez <kramirez101@student.rccd.edu>
2022-04-24 22:31:21 -0700

    uhhh idk what's going on

---
+++ Lab/CIS7_Lab09/Problem01/main.cpp
@@ -0,0 +1,17 @@
+//Fibonacci Sequence using Recursion
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int fib(int n){
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    int n = 11;
+    cout << fib(n);
+    return 0;
+}
